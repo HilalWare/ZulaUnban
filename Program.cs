34569703c3fee5ccc8af10d0f937f4cda6cf62cb
HilalS <79361185+HilalWare@users.noreply.github.com>
2022-06-17 20:41:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Win32;
+using System;
+
+
+
+namespace UnBanZula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RegistryKey key  = Registry.LocalMachine.OpenSubKey(@"HARDWARE\DESCRIPTION\System\CentralProcessor\0", true);
+
+
+            key.DeleteValue("ProcessorNameString");
+
+
+
+
+            key.SetValue("ProcessorNameString", "Amd 9 12212x");
+
+
+
+
+
+
+        }
+    }
+
+}
